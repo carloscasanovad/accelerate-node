c23490952bba02ffec106e8845b3055b3ad4aa55
carloscasanovad <113132910+carloscasanovad@users.noreply.github.com>
2022-12-21 15:12:03 -0300

    feat/EV-1077/adding intefaces

--- src/application/controllers/CustomerController.ts
+++ src/application/controllers/CustomerController.ts
@@ -1,13 +1,18 @@
 import { Response, Request } from "express";
-import { Customer } from "@domain/user/model/CustomerModel";
 import { CustomerInfo } from "@/protocols";
+import { UserRepository } from "../repositories/UserRepository";
 import httpStatus from "http-status";
 
-
 export class CustomerController {
+  private userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+
   createUser(req: Request, res: Response) {
     const customerInfo = req.body as CustomerInfo;
-    const newCustomer = new Customer(customerInfo);
+    const newCustomer = this.userRepository.createUser(customerInfo);
     return res
       .status(httpStatus.CREATED)
       .send({ status: "success", customer: newCustomer });
--- src/application/repositories/UserRepository.ts
+++ src/application/repositories/UserRepository.ts
@@ -1,13 +1,15 @@
 import { Customer } from "@/domain/user/model/CustomerModel";
 import { CustomerInfo } from "@/protocols";
+import "reflect-metadata";
+import { container, injectable, inject } from "tsyringe";
 
 export interface IUserRepository {
-  createUser(data: CustomerInfo): Promise<Customer>;
+  createUser(customerInfo: CustomerInfo): Customer;
 }
 
 export class UserRepository implements IUserRepository {
+  createUser(customerInfo: CustomerInfo): Customer {
     //TODO: create user on database
-  createUser(data: CustomerInfo): Promise<Customer> {
-    throw new Error("Method not implemented.");
+    return customerInfo;
   }
 }
